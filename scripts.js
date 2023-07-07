1ca3567128a7374f1f5cadc7b90932809c623e71
Thomas Truett <thomastruett@users.noreply.github.com>
2023-07-07 11:18:54 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,49 +11,72 @@ window.initMap = function() {
 }
 
 function fetchAddresses() {
-    let googleSheetsUrl = `https://sheets.googleapis.com/v4/spreadsheets/1Sp69YxPV2-_NmjjjwaiFCvl59_KeXgLxxHxVingMSK0/values/Sheet1!A1:A?key=AIzaSyCuE37rnuC_y9OlVzhXN3nhjgaPYDK4hcU`;
+    let googleSheetsUrl = `https://sheets.googleapis.com/v4/spreadsheets/1Sp69YxPV2-_NmjjjwaiFCvl59_KeXgLxxHxVingMSK0/values/Sheet1!A1:B?key=AIzaSyCuE37rnuC_y9OlVzhXN3nhjgaPYDK4hcU`;
 
     fetch(googleSheetsUrl)
         .then(response => response.json())
         .then(data => {
-            let addresses = data.values.map(item => item[0]); 
+            let addresses = data.values.map(item => [item[0], parseInt(item[1])]);
             geocodeAddresses(addresses);
         })
         .catch(error => console.error('Error:', error));
 }
 
+
 function geocodeAddresses(addresses) {
-    addresses.forEach(address => {
+    addresses.forEach(item => {
+        const address = item[0];
+        const year = parseInt(item[1]);
+
         if (!storedAddresses[address]) {
-            geocoder.geocode({'address': address}, function(results, status) {
+            geocoder.geocode({ 'address': address }, function (results, status) {
                 if (status === 'OK') {
                     storedAddresses[address] = results[0].geometry.location;
                     sessionStorage.setItem('addresses', JSON.stringify(storedAddresses));
-                    addMarker(results[0].geometry.location);
+                    addMarker(results[0].geometry.location, year);
                 } else {
                     alert('Geocode was not successful for the following reason: ' + status);
                 }
             });
         } else {
-            addMarker(storedAddresses[address]);
+            addMarker(storedAddresses[address], year);
         }
     });
 }
 
-function addMarker(location) {
+
+function addMarker(location, year) {
+    let markerIcon;
+    const markerPosition = new google.maps.LatLng(location.lat, location.lng);
+
+    // Set marker icon based on the year
+    if (year >= 2019 && year <= 2023) {
+        markerIcon = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png';
+    } else if (year >= 2014 && year <= 2018) {
+        markerIcon = 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png';
+    } else {
+        markerIcon = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
+    }
 
     const marker = new google.maps.Marker({
-        position: location,
+        position: markerPosition,
         map: map,
-        icon: 'https://i.ibb.co/ChQL9FQ/frank.png'
+        icon: markerIcon,
+        optimized: false
     });
 
     // Extend the bounds to include the new marker's position
-    bounds.extend(marker.position);
+    bounds.extend(marker.getPosition());
 
     // Fit the map to the new bounds. This will zoom in/out and pan the map as necessary.
     map.fitBounds(bounds);
+
+    // Fallback for custom marker icon
+    marker.addListener('iconerror', function() {
+        marker.setIcon('https://i.ibb.co/ChQL9FQ/frank.png');
+    });
 }
+
 
 function loadScript(url, callback) {
     let script = document.createElement("script");
